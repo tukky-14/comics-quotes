027cd535d895361fda01b1dd1eb049d7499aa607
Eiji Otsuki <bigoonage0104@gmail.com>
2020-05-19 20:06:30 +0900

    fix users/show.html.haml

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
-    @nickname = current_user.nickname
+    @nickname = user.nickname
     @quotes = user.quotes.page(params[:page]).per(5).order("created_at DESC")
   end
 end
